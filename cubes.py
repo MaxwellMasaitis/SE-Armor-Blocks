30235488230824bc2afe9436511249186d7799bc
MaxwellMasaitis <maxwelljmasaitis@gmail.com>
2023-04-05 17:32:03 -0400

    Add edges to cube class

--- cubes.py
+++ cubes.py
@@ -111,6 +111,7 @@ class Face():
             return False
         if self.facekind != obj.facekind:
             return False
+        # TODO: may be the source of the duplicated cube types?
         if self.remainingBudget != obj.remainingBudget:
             return False
         if [self.a, self.b, self.c, self.d] == [obj.a, obj.b, obj.c, obj.d]:
@@ -135,34 +136,162 @@ class Cube():
         self.back = back
         self.left = left
         self.right = right
+
+        self.topBackEdge = None
+        self.topLeftEdge = None
+        self.topFrontEdge = None
+        self.topRightEdge = None
+
+        self.backLeftEdge = None
+        self.frontLeftEdge = None
+        self.frontRightEdge = None
+        self.backRightEdge = None
+
+        self.bottomBackEdge = None
+        self.bottomLeftEdge = None
+        self.bottomFrontEdge = None
+        self.bottomRightEdge = None
+
     def __eq__(self, obj: object) -> bool:
         # unhashable
         #if Counter(self.get_face_list()) != Counter(obj.get_face_list()):
         #   return False
         for cube in [obj, obj.bottom_to_top(), obj.front_to_top(), obj.back_to_top(), obj.left_to_top(), obj.right_to_top()]:
             for rota in [cube, cube.rotate_top_face_90(), cube.rotate_top_face_90().rotate_top_face_90(), cube.rotate_top_face_90().rotate_top_face_90().rotate_top_face_90()]:
-                if self.get_face_list() == rota.get_face_list():
+                if self.get_face_list() == rota.get_face_list() and self.get_edge_list() == rota.get_edge_list():
                     return True
         return False
     def __ne__(self, obj: object) -> bool:
         return not self == obj
     def get_face_list(self) -> list:
         return [self.top, self.bottom, self.front, self.back, self.left, self.right]
+    def get_edge_list(self) -> list:
+        return [self.topBackEdge, self.topLeftEdge, self.topFrontEdge, self.topRightEdge, self.backLeftEdge, self.backRightEdge, self.frontLeftEdge, self.frontRightEdge,
+                self.bottomBackEdge, self.bottomLeftEdge, self.bottomFrontEdge, self.bottomRightEdge]
     # set a face to be the top, then check all 4 rotations
     # return Cube(self.top, self.bottom, self.front, self.back, self.left, self.right)
     def rotate_top_face_90(self):
-        return Cube(self.top, self.bottom, self.right, self.left, self.front, self.back)
+        # clockwise rotation
+        cube = Cube(self.top, self.bottom, self.right, self.left, self.front, self.back)
+
+        cube.topBackEdge = self.topLeftEdge
+        cube.topLeftEdge = self.topFrontEdge
+        cube.topFrontEdge = self.topRightEdge
+        cube.topRightEdge = self.topBackEdge
+
+        cube.backLeftEdge = self.frontLeftEdge
+        cube.frontLeftEdge = self.frontRightEdge
+        cube.frontRightEdge = self.backRightEdge
+        cube.backRightEdge = self.backLeftEdge
+
+        cube.bottomBackEdge = self.bottomLeftEdge
+        cube.bottomLeftEdge = self.bottomFrontEdge
+        cube.bottomFrontEdge = self.bottomRightEdge
+        cube.bottomRightEdge = self.bottomBackEdge
+
+        return cube
     
+    # all these could be just front_to_top and rotate_top_face_90 in various combinations...
     def bottom_to_top(self):
-        return Cube(self.bottom, self.top, self.back, self.front, self.left, self.right)
+        # could also do front_to_top twice. or anything twice?
+        cube = Cube(self.bottom, self.top, self.back, self.front, self.left, self.right)
+
+        cube.topBackEdge = self.bottomFrontEdge
+        cube.topLeftEdge = self.bottomLeftEdge
+        cube.topFrontEdge = self.bottomBackEdge
+        cube.topRightEdge = self.bottomRightEdge
+
+        cube.backLeftEdge = self.frontLeftEdge
+        cube.frontLeftEdge = self.backLeftEdge
+        cube.frontRightEdge = self.backRightEdge
+        cube.backRightEdge = self.frontRightEdge
+
+        cube.bottomBackEdge = self.topFrontEdge
+        cube.bottomLeftEdge = self.topLeftEdge
+        cube.bottomFrontEdge = self.topBackEdge
+        cube.bottomRightEdge = self.topRightEdge
+        
+        return cube
+    
     def front_to_top(self):
-        return Cube(self.front, self.back, self.bottom, self.top, self.left, self.right)
+        cube = Cube(self.front, self.back, self.bottom, self.top, self.left, self.right)
+
+        cube.topBackEdge = self.topFrontEdge
+        cube.topLeftEdge = self.frontLeftEdge
+        cube.topFrontEdge = self.bottomFrontEdge
+        cube.topRightEdge = self.frontRightEdge
+
+        cube.backLeftEdge = self.topLeftEdge
+        cube.frontLeftEdge = self.bottomLeftEdge
+        cube.frontRightEdge = self.bottomRightEdge
+        cube.backRightEdge = self.topRightEdge
+
+        cube.bottomBackEdge = self.topBackEdge
+        cube.bottomLeftEdge = self.backLeftEdge
+        cube.bottomFrontEdge = self.bottomBackEdge
+        cube.bottomRightEdge = self.backRightEdge
+        
+        return cube
+    
     def back_to_top(self):
-        return Cube(self.back, self.front, self.top, self.bottom, self.left, self.right)
+        cube = Cube(self.back, self.front, self.top, self.bottom, self.left, self.right)
+
+        cube.topBackEdge = self.bottomBackEdge
+        cube.topLeftEdge = self.backLeftEdge
+        cube.topFrontEdge = self.topBackEdge
+        cube.topRightEdge = self.backRightEdge
+
+        cube.backLeftEdge = self.bottomLeftEdge
+        cube.frontLeftEdge = self.topLeftEdge
+        cube.frontRightEdge = self.topRightEdge
+        cube.backRightEdge = self.bottomRightEdge
+
+        cube.bottomBackEdge = self.bottomFrontEdge
+        cube.bottomLeftEdge = self.frontLeftEdge
+        cube.bottomFrontEdge = self.topFrontEdge
+        cube.bottomRightEdge = self.frontRightEdge
+        
+        return cube
+    
     def left_to_top(self):
-        return Cube(self.left, self.right, self.front, self.back, self.bottom, self.top)
+        cube = Cube(self.left, self.right, self.front, self.back, self.bottom, self.top)
+
+        cube.topBackEdge = self.backLeftEdge
+        cube.topLeftEdge = self.bottomLeftEdge
+        cube.topFrontEdge = self.frontLeftEdge
+        cube.topRightEdge = self.topLeftEdge
+
+        cube.backLeftEdge = self.bottomBackEdge
+        cube.frontLeftEdge = self.bottomFrontEdge
+        cube.frontRightEdge = self.topFrontEdge
+        cube.backRightEdge = self.topBackEdge
+
+        cube.bottomBackEdge = self.backRightEdge
+        cube.bottomLeftEdge = self.bottomRightEdge
+        cube.bottomFrontEdge = self.frontRightEdge
+        cube.bottomRightEdge = self.topRightEdge
+        
+        return cube
+    
     def right_to_top(self):
-        return Cube(self.right, self.left, self.front, self.back, self.top, self.bottom)
+        cube = Cube(self.right, self.left, self.front, self.back, self.top, self.bottom)
+
+        cube.topBackEdge = self.backRightEdge
+        cube.topLeftEdge = self.topRightEdge
+        cube.topFrontEdge = self.frontRightEdge
+        cube.topRightEdge = self.bottomRightEdge
+
+        cube.backLeftEdge = self.topBackEdge
+        cube.frontLeftEdge = self.topFrontEdge
+        cube.frontRightEdge = self.bottomFrontEdge
+        cube.backRightEdge = self.bottomBackEdge
+
+        cube.bottomBackEdge = self.backLeftEdge
+        cube.bottomLeftEdge = self.topLeftEdge
+        cube.bottomFrontEdge = self.frontLeftEdge
+        cube.bottomRightEdge = self.bottomLeftEdge
+        
+        return cube
     
     def areFacesValid(self) -> bool:
         okFaceCount = 0
@@ -172,6 +301,36 @@ class Cube():
             elif face is None:
                 okFaceCount += 1
         return okFaceCount == 6
+    
+    def addEdge(self, faces: tuple, edge: Edge) -> None:
+        if ("top" in faces):
+            if ("back" in faces):
+                self.topBackEdge = edge
+            elif ("left" in faces):
+                self.topLeftEdge = edge
+            elif ("front" in faces):
+                self.topFrontEdge = edge
+            elif ("right" in faces):
+                self.topRightEdge = edge
+        elif ("bottom" in faces):
+            if ("back" in faces):
+                self.bottomBackEdge = edge
+            elif ("left" in faces):
+                self.bottomLeftEdge = edge
+            elif ("front" in faces):
+                self.bottomFrontEdge = edge
+            elif ("right" in faces):
+                self.bottomRightEdge = edge
+        elif ("back" in faces):
+            if ("left" in faces):
+                self.backLeftEdge = edge
+            elif ("right" in faces):
+                self.backRightEdge = edge
+        elif ("front" in faces):
+            if ("left" in faces):
+                self.frontLeftEdge = edge
+            elif ("right" in faces):
+                self.frontRightEdge = edge
 
 def cullCubes(cubeList: list) -> list:
     tempCubes = []
@@ -200,6 +359,7 @@ def recursiveEdgeCheck(cube: Cube, newestFace: str, connectedFaces: list):
         nextCube = copy.deepcopy(cube)
         nextCubeNewestFace = getattr(nextCube, newestFace)
         nextCubeTargetFace =  getattr(nextCube, targetTuple[0])
+        nextCube.addEdge((newestFace, targetTuple[0]), edge)
         #nextCubeTargetFace.x = edge
         setattr(nextCubeTargetFace, targetTuple[1], edge)
         nextCubeTargetFace.remainingBudget.remove(edge)
